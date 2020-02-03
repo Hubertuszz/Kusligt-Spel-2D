084fe3a38fae92603eac91eabff1fa4100ebdf7d
luxen1337 <45816345+luxen1337@users.noreply.github.com>
2020-02-03 14:23:44 +0100

    Attacking completed

---
+++ Assets/Scripts/Player/General Scripts/Attack.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public int dmg;
+    bool canAttack = false;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if(canAttack == true && Input.GetKeyDown(KeyCode.Space))
+            Debug.Log("ATTACKING"); // Time to attack
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Infected")
+            canAttack = true;
+    }
+
+    void OnTriggerExit2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "Infected")
+            canAttack = false;
+    }
+}
