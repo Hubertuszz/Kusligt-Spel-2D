fe5670e41277febd7110b1b3d565b75fa173cc2c
luxen1337 <45816345+luxen1337@users.noreply.github.com>
2020-02-03 13:25:05 +0100

    Main character fixes

---
+++ Software Inc/Assets/Scripts/Player/General Scripts/ChangeAppearance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAppearance : MonoBehaviour
+{
+    public Sprite[] sprites;
+    bool isInfected = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.V) && isInfected == false) {
+            GetComponent<SpriteRenderer>().sprite = sprites[0];
+            isInfected = true;
+            
+        } else if (Input.GetKeyDown(KeyCode.V) && isInfected == true) {
+            GetComponent<SpriteRenderer>().sprite = sprites[1];
+            isInfected = false;
+        }
+    }
+
+}
