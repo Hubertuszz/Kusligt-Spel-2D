16204697148240ba831d713b4fe4173a65293d21
Hubertuszz <hugo.mattsson@elev.ga.klaragymnasium.se>
2020-02-03 12:34:51 +0100

    Nytt Projekt

---
+++ Software Inc/Assets/Scripts/Enemies/EnemyCR/CloseRangeAI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseRangeAI : MonoBehaviour
+{
+    private StateMachine sm = new StateMachine();
+
+    ////public variables
+    public GameObject target;
+    public Transform forwardPos;
+    public Transform backPos;
+
+    ////private variables
+    private Rigidbody2D rb;
+    private bool fwtrue = true;
+    private bool facingRight = false;
+
+    //Radius
+    private float moveRad;
+
+    //movement
+    private float maxSpeed = 7.5f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        //States
+        sm.AddState("Idle", idle);
+        sm.SetActiveState("Idle");
+        
+    }
+
+    void Update()
+    {
+        sm.Run();
+    }
+
+    public void idle(StateMachine s)
+    {
+        if (Vector3.Distance(transform.position, target.transform.position) < moveRad)
+        {
+            //setactivestate movement
+
+
+        }
+        else if (transform.position.x > forwardPos.transform.position.x && fwtrue == true)
+        {
+            rb.velocity = new Vector2(-maxSpeed, rb.velocity.y);
+            if (facingRight == true)
+            {
+                transform.Rotate(0, 180, 0);
+                facingRight = false;
+            }
+        }
+        else
+        {
+            fwtrue = false;
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+            if (facingRight != true)
+            {
+                transform.Rotate(0, 180, 0);
+                facingRight = true;
+            }
+            if(transform.position.x > backPos.transform.position.x)
+            {
+                
+                fwtrue = true;
+            }
+        }
+        
+    }
+}
---
+++ Software Inc/Assets/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateMachine
+{
+    public delegate void StateAction(StateMachine sm);
+
+    private Dictionary<string, StateAction> states = new Dictionary<string, StateAction>();
+    private StateAction activeStateAction = null;
+
+    public int AddState(string stateName, StateAction stateAction)
+    {
+        if (!states.ContainsKey(stateName))
+        {
+            states.Add(stateName, stateAction);
+            return 0;
+        }
+        else
+            return -1;
+    }
+
+    public void Reset()
+    {
+        activeStateAction = null;
+    }
+
+    public int SetActiveState(string stateName)
+    {
+        StateAction stateAction;
+        if (states.TryGetValue(stateName, out stateAction))
+        {
+            activeStateAction = stateAction;
+            return 0;
+        }
+        else
+            return -1;
+    }
+
+    public int Run()
+    {
+        if (activeStateAction == null)
+        {
+            return -1;
+        }
+        activeStateAction(this);
+        return 0;
+    }
+}
