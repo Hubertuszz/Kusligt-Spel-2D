ab853333510c879d73726ec5903f9a84643b68ed
Thaininja69 <peterekholm31@gmail.com>
2020-04-20 13:41:55 +0200

    lvl1

---
+++ Assets/Sprites/Scripts/Player/General Scripts/ChangeAppearance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ChangeAppearance : MonoBehaviour
+{
+    public Sprite[] sprites;
+    public bool isInfected = false;
+    public Image image;
+    public float amount = 0;
+
+    int ChangedCount =0;
+    // Start is called before the first frame update
+    void Start() {
+        isInfected = false;
+        amount = 0;
+    }
+    void Update() {
+        image.rectTransform.sizeDelta = new Vector2(amount,10);
+        
+        if(isInfected == false) {
+            if(amount < 100f)
+                amount += 0.2f;
+        } else
+        {
+            if (amount >= 0)
+                amount-=0.1f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.P) && isInfected == false) {
+            if(ChangedCount == 0 && amount >= 100 || ChangedCount > 0) {
+                GetComponent<SpriteRenderer>().sprite = sprites[1];
+                isInfected = true;
+                GetComponent<Attack>().enabled = true;
+            }
+        } else if (amount <= 0 && isInfected == true || Input.GetKeyDown(KeyCode.P)) {
+            GetComponent<SpriteRenderer>().sprite = sprites[0];
+            isInfected = false;
+            GetComponent<Attack>().enabled = false;
+        }
+    }
+}
---
+++ Assets/Sprites/Scripts/Player/General Scripts/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int health;
+    public Image image;
+    void Start() {
+        health = 100;
+    }
+    
+    void Update() {
+        image.rectTransform.sizeDelta = new Vector2(health, 20);
+        if (health <= 0)
+            Die(); 
+    }
+
+   public void Damage(int a) {
+        health -= a;
+    }
+
+    void Die() {
+        Destroy(gameObject);
+    }
+}
