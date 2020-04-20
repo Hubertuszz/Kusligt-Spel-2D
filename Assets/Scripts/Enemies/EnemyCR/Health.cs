b1b3ae49387998c73cc1f478696cc450d9cd46b1
Thaininja69 <peterekholm31@gmail.com>
2020-04-20 14:02:40 +0200

    pull

---
+++ Assets/Scripts/Enemies/EnemyCR/Health.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int health= 100;
+    public GameObject Player;
+    // Start is called before the first frame update
+    public void TakeDmg(int amount) {
+        health -= amount;
+        if(health <= 0) {
+            Player.GetComponent<ChangeAppearance>().amount = 100;
+            Destroy(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
